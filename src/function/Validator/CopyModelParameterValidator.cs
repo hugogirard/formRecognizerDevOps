ca26464e0cb00e55e5f9c91d61aef34859903955
hugogirard <hugo.girard@microsoft.com>
2022-01-06 10:36:04 -0500

    added validation copy model

--- src/function/Validator/CopyModelParameterValidator.cs
+++ src/function/Validator/CopyModelParameterValidator.cs
@@ -5,5 +5,8 @@ public class CopyModelParameterValidator : AbstractValidator<CopyModelParameter>
     public CopyModelParameterValidator()
     {
         RuleFor(p => p.SourceModelId).NotEmpty().NotNull();
+        RuleFor(p => p.SourceEnvironment).IsInEnum();
+        RuleFor(p => p.DestinationEnvironment).IsInEnum();
+        RuleFor(p => p.DestinationEnvironment).NotEqual(p => p.SourceEnvironment);
     }
 }
